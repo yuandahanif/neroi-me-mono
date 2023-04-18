97b0c0a7f969638986b406e8d4730a9cef90b539
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2023-04-18 15:19:57 +0700

    feat: :sparkles: add click outside hook

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,23 @@
+import { type MutableRefObject, useEffect } from "react";
+
+function useOnClickOutside(
+  ref: MutableRefObject<HTMLElement | null>,
+  handler: (e: MouseEvent | TouchEvent) => void
+) {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event?.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+}
+
+export default useOnClickOutside;
