5f247241b4f3e0c16b798f368136674b3bc912c5
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2023-05-02 13:32:55 +0700

    feat: :sparkles: fix mobil layout for blog page

--- src/components/navigation/main.navigation.tsx
+++ src/components/navigation/main.navigation.tsx
@@ -90,11 +90,31 @@ const MainNavigation = () => {
               ref={navigationref}
               className="min-h-96 fixed left-1/2 top-1/2 z-50 m-auto w-full max-w-screen-md -translate-x-1/2 -translate-y-1/2 overflow-auto border bg-main-600 p-5"
             >
-              <span>Navigasi</span>
               <div>
-                <ul>
+                <ul className="flex flex-col gap-4">
                   <li>
-                    <span>Todo: the navigation</span>
+                    <a
+                      href="https://github.com/yuandahanif/neroi-me-mono"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex justify-center gap-1 hover:underline"
+                    >
+                      <span>View source code</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.2}
+                        stroke="currentColor"
+                        className="h-4 w-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                        />
+                      </svg>
+                    </a>
                   </li>
                 </ul>
               </div>
