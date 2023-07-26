2a6cfaeb7fc1c3771987d1e830d140fa43bd700a
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2023-07-27 01:42:03 +0700

    feat: :sparkles: add cv

--- src/pages/me/resume.tsx
+++ src/pages/me/resume.tsx
@@ -7,14 +7,28 @@ const ResumePage: NextPage = () => {
     <>
       <HeadSEO />
       <MainLayout>
-        <div className="flex h-screen w-full justify-center">
-          <iframe
-            className="h-full border"
-            width="800"
-            height="450"
-            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FUwVs2OBi9hYhhxleQXEkE2%2FResume%3Fnode-id%3D1%253A210%26t%3D7YxDGs0ehA3pOOZZ-1"
-            allowFullScreen
-          ></iframe>
+        <div className="py-9">
+          <div className="mx-auto mb-4 w-fit border p-4">
+            <embed
+              src="/files/Yuanda-Hanif Hisyam-CV.pdf"
+              width="800"
+              height="450"
+              className=""
+            ></embed>
+            <a href="/files/Yuanda-Hanif Hisyam-CV.pdf" download className="hover:underline">
+              Download my CV
+            </a>
+          </div>
+
+          <div className="flex h-screen w-full justify-center">
+            <iframe
+              className="h-full border"
+              width="800"
+              height="450"
+              src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FUwVs2OBi9hYhhxleQXEkE2%2FResume%3Fnode-id%3D1%253A210%26t%3D7YxDGs0ehA3pOOZZ-1"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       </MainLayout>
     </>
