0037af8f634c07e79222f6ac029233207883d71a
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2023-12-25 10:22:05 +0700

    Revert "test"
    This reverts commit 3765a16aba0d5e27ee04e96972f77732d5d19db6.

--- script/refactor.sh
+++ script/refactor.sh
@@ -2,11 +2,9 @@
 
 if [ "$1" = "prepare" ]; then
     mv $PWD/src/pages $PWD/src/pages-old
-    git reset $PWD/src/pages
     echo "Folder 'pages' renamed to 'pages-old'"
 elif [ "$1" = "restore" ]; then
     mv $PWD/src/pages-old $PWD/src/pages
-    git reset $PWD/src/pages-old
     echo "Folder 'pages-old' renamed to 'pages'"
 else
     echo "Invalid parameter. Usage: ./refactor.sh [prepare|restore]"
