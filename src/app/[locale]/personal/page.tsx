dc487782995b9c64fc3e04178f834e25814c36fb
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2023-12-27 17:19:34 +0700

    Add language switcher

--- src/app/[locale]/personal/page.tsx
+++ src/app/[locale]/personal/page.tsx
@@ -1,38 +1,19 @@
 import { getI18n } from "~/locales/server";
+import PersonalClientSection from "./_client";
+import Logo from "~/components/logo/logo";
 
-const Home = async () => {
-  const t = await getI18n();
-
+const Home = () => {
   return (
     <main
       className={`flex min-h-screen grow flex-col items-center justify-center`}
     >
-      <h1>{t("Quote")}</h1>
-      {/* <div className="">
-          <Logo />
-        </div> */}
+      <div className="">
+        <Logo />
+      </div>
 
       {/* <MainNavigation /> */}
 
-      <div className="prose prose-sm prose-invert mt-10 px-4 text-center lg:px-0">
-        {/* <p ref={quoteRef} data-quote={LL.Quote()} /> */}
-      </div>
-
-      <div className="mt-10 flex gap-2 target:animate-bounce" id="change_lang">
-        {/* {locales.map((locale_, idx) => (
-            <div key={locale_} className="flex gap-2">
-              <button
-                type="button"
-                className={twMerge("hover:underline")}
-                onClick={() => void chanegLanguage(locale_)}
-                disabled={locale_ == locale}
-              >
-                {locale_ == locale ? <Redacted>{locale_}</Redacted> : locale_}
-              </button>
-              {idx !== locales.length - 1 && <span>|</span>}
-            </div>
-          ))} */}
-      </div>
+      <PersonalClientSection />
     </main>
   );
 };
