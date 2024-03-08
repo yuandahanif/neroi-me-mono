5c3971ea3c0368e355470f2607da3f74b6161cc9
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-03-08 20:15:36 +0700

    update MD embbed & image host

--- src/app/[locale]/(not-subdomain)/admin/blogs/preview/route.ts
+++ src/app/[locale]/(not-subdomain)/admin/blogs/preview/route.ts
@@ -1,15 +1,19 @@
 "use server";
 
 import { serialize } from "next-mdx-remote/serialize";
+import remarkUnwrapImages from "remark-unwrap-images";
 import { auth } from "~/server/auth";
 
 export async function POST(request: Request) {
   try {
     await auth();
     const mdx = await request.text();
-    const source = await serialize(mdx);
+    const source = await serialize(mdx, {
+      mdxOptions: { remarkPlugins: [remarkUnwrapImages] },
+    });
     return Response.json({ ...source });
   } catch (error) {
+    console.error(error);
     throw Response.error();
   }
 }
